2a4cc0d9951573806efe728460dc1f657c124eac
Cabelinho-hub <cabelinhokwon@gmail.com>
2026-05-04 02:27:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ ID_CANAL_LOGS = 1417278749497364550
 
 CHAVE_IA = os.environ.get("GEMINI_KEY")
 genai.configure(api_key=os.environ.get("GEMINI_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash') 
+
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest") 
 # --- FUNÇÕES DE SUPORTE ---
 
 def baixar_video_link(url):
